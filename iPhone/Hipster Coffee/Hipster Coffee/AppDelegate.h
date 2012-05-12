645a8f11985a0e7cd3b60d923205493a08f0ad1e
Emre Berge Ergenekon <eergenekon@splunk.com>
2012-05-12 09:30:16 +0200

    Added Map View

--- iPhone/Hipster Coffee/Hipster Coffee/AppDelegate.h
+++ iPhone/Hipster Coffee/Hipster Coffee/AppDelegate.h
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.h
-//  Hipster Coffee
-//
-//  Created by Emre Ergenekon on 5/12/12.
-//  Copyright (c) 2012 Splunk, Bontouch AB, Codely HB. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
--- iPhone/Hipster Coffee/Hipster Coffee/AppDelegate.m
+++ iPhone/Hipster Coffee/Hipster Coffee/AppDelegate.m
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.m
-//  Hipster Coffee
-//
-//  Created by Emre Ergenekon on 5/12/12.
-//  Copyright (c) 2012 Splunk, Bontouch AB, Codely HB. All rights reserved.
-//
-
 #import "AppDelegate.h"
 
 @implementation AppDelegate
