5f09abc372b18d426dc311983730af9a290f3f14
Simon Lundmark <simon.lundmark@gmail.com>
2012-05-12 13:03:43 +0200

    Removed some copyright notice shizzle comments

--- iPhone/Hipster Coffee/Hipster Coffee/VenueViewController.h
+++ iPhone/Hipster Coffee/Hipster Coffee/VenueViewController.h
@@ -1,11 +1,3 @@
-//
-//  VenueViewController.h
-//  Hipster Coffee
-//
-//  Created by Emre Ergenekon on 5/12/12.
-//  Copyright (c) 2012 Splunk, Bontouch AB, Codely HB. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface VenueViewController : UITableViewController
--- iPhone/Hipster Coffee/Hipster Coffee/VenueViewController.m
+++ iPhone/Hipster Coffee/Hipster Coffee/VenueViewController.m
@@ -1,11 +1,3 @@
-//
-//  VenueViewController.m
-//  Hipster Coffee
-//
-//  Created by Emre Ergenekon on 5/12/12.
-//  Copyright (c) 2012 Splunk, Bontouch AB, Codely HB. All rights reserved.
-//
-
 #import "VenueViewController.h"
 
 @interface VenueViewController ()
