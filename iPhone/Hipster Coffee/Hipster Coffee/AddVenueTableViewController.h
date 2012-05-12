d65b3c9de3ea959ab22b66bdb35a19c9ed3da141
Emre Berge Ergenekon <eergenekon@splunk.com>
2012-05-12 09:42:50 +0200

    Adding add venue table view controller

---
+++ iPhone/Hipster Coffee/Hipster Coffee/AddVenueTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddVenueTableViewController.h
+//  Hipster Coffee
+//
+//  Created by Emre Ergenekon on 5/12/12.
+//  Copyright (c) 2012 Splunk, Bontouch AB, Codely HB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddVenueTableViewController : UITableViewController
+
+@end
